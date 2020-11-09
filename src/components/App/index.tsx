ee7e613bd50b69bf9242663f3a909c16d4a298af
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-11-09 15:51:52 +0100

    Implement fetching

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,3 +1,27 @@
 import * as React from 'react';
+import {
+  getRepositoriesByStars,
+  GetRepositoriesByStarsResponse,
+} from '../../api/github';
+import { RepositoryList, Row } from '../RepositoryList';
 
-export const App: React.FC = () => <>Loading...</>;
+export const App: React.FC = () => {
+  const [repositories, setRepositories] = React.useState<
+    GetRepositoriesByStarsResponse
+  >();
+
+  React.useEffect(() => {
+    getRepositoriesByStars().then(setRepositories);
+  }, []);
+
+  if (repositories) {
+    const rows: Row[] = repositories.items.map((item) => ({
+      name: item.full_name,
+      stars: item.stargazers_count,
+    }));
+
+    return <RepositoryList caption="Repositories" rows={rows} />;
+  }
+
+  return <>Loading...</>;
+};
