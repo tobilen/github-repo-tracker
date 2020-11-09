a334825597cc6111e23a3606743d9669010997fa
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-11-09 15:45:31 +0100

    Add test to assert data is fetched

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -1,3 +1,18 @@
+import * as React from 'react';
+import fetchMock from 'fetch-mock-jest';
+import { render, waitFor } from '@testing-library/react';
+import { getRepositoriesUrl } from '../../api/github';
+import { getRepositoriesByStarsResponse } from '../../mocks/github';
+import { App } from './index';
+
 describe('App', () => {
-  it('fetches repositories from github', () => {});
+  it('fetches repositories from github', async () => {
+    fetchMock.get(getRepositoriesUrl, getRepositoriesByStarsResponse);
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveGot(getRepositoriesUrl);
+    });
+  });
 });
