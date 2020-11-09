7bd55dfcb4ad9f65683c2c2c9652d759c9d33412
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-11-09 15:28:11 +0100

    Add App component

---
+++ src/components/App/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import { App } from './index';
+
+export default {
+  title: 'App',
+};
+
+export const Default: Story = () => <App />;
---
+++ src/components/App/index.test.tsx
@@ -0,0 +1,3 @@
+describe('App', () => {
+  it('fetches repositories from github', () => {});
+});
