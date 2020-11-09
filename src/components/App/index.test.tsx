5c3bb3e4724adf26f7141e062912e04d01852abf
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-11-09 16:01:30 +0100

    Add test to make sure loading animation is displayed

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import fetchMock from 'fetch-mock-jest';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import { getRepositoriesUrl } from '../../api/github';
 import { getRepositoriesByStarsResponse } from '../../mocks/github';
 import { App } from './index';
@@ -10,6 +10,19 @@ describe('App', () => {
     fetchMock.get(getRepositoriesUrl, getRepositoriesByStarsResponse);
 
     render(<App />);
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveGot(getRepositoriesUrl);
+    });
+  });
+
+  it('shows a loading view while fetch is inflight', async () => {
+    fetchMock.get(getRepositoriesUrl, getRepositoriesByStarsResponse, {
+      delay: 500,
+    });
+
+    render(<App />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
 
     await waitFor(() => {
       expect(fetchMock).toHaveGot(getRepositoriesUrl);
