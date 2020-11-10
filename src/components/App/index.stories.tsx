d509b6ead5dd1f71fa398cbbd5d0446babf907c9
Tobias Lengsholz <tobias.lengsholz@invision.de>
2020-11-10 23:03:08 +0100

    Mock request in storybook

--- src/components/App/index.stories.tsx
+++ src/components/App/index.stories.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import fetchMock from 'fetch-mock';
+import { getRepositoriesByStarsResponse } from '../../mocks/github';
 import { App } from './index';
 
 export default {
   title: 'App',
 };
 
-export const Default: Story = () => <App />;
+export const Default: Story = () => {
+  fetchMock
+    .restore()
+    .get(
+      'https://api.github.com/search/repositories?q=created:%3E2017-01-10&sort=stars&order=desc',
+      getRepositoriesByStarsResponse,
+    );
+  return <App />;
+};
